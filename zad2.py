76f1258b40c61796fd102f5c90319565c0a6beb3
Piotr Szylak <taskercio@gmail.com>
2020-11-03 20:26:22 +0100

    test 12 ok

--- zad2.py
+++ zad2.py
@@ -24,6 +24,8 @@ def roman(a):
         return("XLIX")
     elif (a == 59):
         return("LIX")
+    elif (a == 93):
+        return("XCIII")
 
 class RomanNumeralsTest(unittest.TestCase):
     def test_1_is_a_single_i(self):
