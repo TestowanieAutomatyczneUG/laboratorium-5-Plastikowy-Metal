30c3ec6fb74d5cc6436717c50999189b620ef09d
Piotr Szylak <taskercio@gmail.com>
2020-11-03 21:25:06 +0100

    test 7 refactor

--- zad3.py
+++ zad3.py
@@ -1,7 +1,7 @@
 import unittest
 
 class Piosenka():
-    def wersy(a, b = "brak"):
+    def wersy(a = "all", b = "brak"):
         song = ["On the first day of Christmas my true love gave to me: a Partridge in a Pear Tree.",
                 "On the second day of Christmas my true love gave to me: two Turtle Doves, and a Partridge in a Pear Tree.",
                 "On the third day of Christmas my true love gave to me: three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
@@ -14,13 +14,16 @@ class Piosenka():
                 "On the tenth day of Christmas my true love gave to me: ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
                 "On the eleventh day of Christmas my true love gave to me: eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
                 "On the twelfth day of Christmas my true love gave to me: twelve Drummers Drumming, eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree."]
-
-        if(type(a) == int and b == "brak"):
-            return(song[a - 1])
-        elif(type(a) == int and type(b) == int):
-            return(song[a-1:b])
+        if(type(a) == int):
+            if (a > 12 or a < 1):
+                raise ValueError("Podany numer wersu nie miesci sie w zakresie")
+            elif(type(a) == int and b == "brak"):
+                return(song[a - 1])
+            elif(type(a) == int and type(b) == int):
+                return(song[a-1:b])
         elif(a == "all" and b == "brak"):
             return(song[0:])
+
 
 
 class PiosenkaTest(unittest.TestCase):
@@ -54,7 +57,7 @@ class PiosenkaTest(unittest.TestCase):
                  "On the twelfth day of Christmas my true love gave to me: twelve Drummers Drumming, eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree."])
 
     def test_6_all_lines(self):
-        self.assertEqual(Piosenka.wersy("all"),
+        self.assertEqual(Piosenka.wersy(),
         ["On the first day of Christmas my true love gave to me: a Partridge in a Pear Tree.",
          "On the second day of Christmas my true love gave to me: two Turtle Doves, and a Partridge in a Pear Tree.",
          "On the third day of Christmas my true love gave to me: three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
@@ -67,10 +70,11 @@ class PiosenkaTest(unittest.TestCase):
          "On the tenth day of Christmas my true love gave to me: ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
          "On the eleventh day of Christmas my true love gave to me: eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
          "On the twelfth day of Christmas my true love gave to me: twelve Drummers Drumming, eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree."])
-    @unittest.skip
+
     def test_7_line_is_out_of_range(self):
         with self.assertRaisesWithMessage(ValueError):
-            Piosenka.wersy(15)
+            Piosenka.wersy(13)
+
     @unittest.skip
     def test_8_first_range_bigger(self):
         with self.assertRaisesWithMessage(ValueError):
