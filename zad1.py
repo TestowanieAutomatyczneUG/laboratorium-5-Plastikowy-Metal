84f9240b4d14d602974b885c31b1fd59e7ccd4a8
Piotr Szylak <taskercio@gmail.com>
2020-11-03 20:02:46 +0100

    test 5 failure

--- zad1.py
+++ zad1.py
@@ -26,7 +26,6 @@ class HammingTest(unittest.TestCase):
     def test_long_identical_strands(self):
         self.assertEqual(hamming.distance("GGACTGAAATCTG", "GGACTGAAATCTG"), 0)
 
-    @unittest.skip
     def test_long_different_strands(self):
         self.assertEqual(hamming.distance("GGACGGATTCTG", "AGGACGGATTCT"), 9)
 
