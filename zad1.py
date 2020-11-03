e3674561cd3195e879a36b32969e3ac8a2a534ff
Piotr Szylak <taskercio@gmail.com>
2020-11-03 20:09:33 +0100

    test 8 ok

--- zad1.py
+++ zad1.py
@@ -44,12 +44,10 @@ class HammingTest(unittest.TestCase):
         with self.assertRaisesWithMessage(ValueError):
             hamming.distance("ATA", "AGTG")
 
-    @unittest.skip
     def test_disallow_left_empty_strand(self):
         with self.assertRaisesWithMessage(ValueError):
             hamming.distance("", "G")
 
-    @unittest.skip
     def test_disallow_right_empty_strand(self):
         with self.assertRaisesWithMessage(ValueError):
             hamming.distance("G", "")
