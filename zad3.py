e93debe4daa00ccbd6d7241cf00629580a58fdbd
Piotr Szylak <taskercio@gmail.com>
2020-11-03 21:21:01 +0100

    test 6 failure

--- zad3.py
+++ zad3.py
@@ -20,7 +20,6 @@ class Piosenka():
         elif(type(a) == int and type(b) == int):
             return(song[a-1:b])
 
-
 class PiosenkaTest(unittest.TestCase):
     def test_1_line_1(self):
         self.assertEqual(Piosenka.wersy(1),
@@ -50,9 +49,9 @@ class PiosenkaTest(unittest.TestCase):
                  "On the tenth day of Christmas my true love gave to me: ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
                  "On the eleventh day of Christmas my true love gave to me: eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
                  "On the twelfth day of Christmas my true love gave to me: twelve Drummers Drumming, eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree."])
-    @unittest.skip
+
     def test_6_all_lines(self):
-        self.assertEqual(Piosenka.wersy(1),
+        self.assertEqual(Piosenka.wersy("all"),
         ["On the first day of Christmas my true love gave to me: a Partridge in a Pear Tree.",
          "On the second day of Christmas my true love gave to me: two Turtle Doves, and a Partridge in a Pear Tree.",
          "On the third day of Christmas my true love gave to me: three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
